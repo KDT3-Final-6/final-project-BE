88c52c0500be362fdf8dde8ea406219c634a4c55
zoomin3022 <zoomin3022@gmail.com>
2023-03-16 14:04:26 +0900

    feat: #5 Global Entity의 생성일 자동 생성을 위해 필요한 어노테이션 추가

--- travel/src/main/java/com/travel/TravelApplication.java
+++ travel/src/main/java/com/travel/TravelApplication.java
@@ -2,12 +2,14 @@ package com.travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TravelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TravelApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TravelApplication.class, args);
+    }
 
 }
