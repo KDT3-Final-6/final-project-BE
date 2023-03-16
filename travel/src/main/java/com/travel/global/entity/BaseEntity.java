a7bd01e6c6ecd34622444eac19813d59069ef1c4
zoomin3022 <zoomin3022@gmail.com>
2023-03-16 12:40:29 +0900

    feat: #5 global 패키지내의 Entity 생성
    생성 시간만 있는 BaseEntity를 기반으로 추가된 기능을 with 뒤에 붙이는 형식으로 만들 것이다 이번 커밋에는 기본과 수정날짜가 추가된 WithModifiedDate 만 생성

---
+++ travel/src/main/java/com/travel/global/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.travel.global.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+
+}
---
+++ travel/src/main/java/com/travel/global/entity/BaseEntityWithModifiedDate.java
@@ -0,0 +1,18 @@
+package com.travel.global.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntityWithModifiedDate extends BaseEntity {
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
