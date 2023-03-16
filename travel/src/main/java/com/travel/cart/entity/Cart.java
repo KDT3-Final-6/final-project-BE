3727cfd8e3f79f24882c153ab6133ac957689d4e
zoomin3022 <zoomin3022@gmail.com>
2023-03-16 12:46:23 +0900

    feat: #5 Cart Entity 생성

---
+++ travel/src/main/java/com/travel/cart/entity/Cart.java
@@ -0,0 +1,30 @@
+package com.travel.cart.entity;
+
+import com.travel.global.entity.BaseEntity;
+import com.travel.member.entity.Member;
+import com.travel.product.entity.Product;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "cart")
+public class Cart extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
