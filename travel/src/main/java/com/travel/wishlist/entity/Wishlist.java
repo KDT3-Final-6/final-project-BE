aaca3d4346efb63702be62c7090fd682588f140d
zoomin3022 <zoomin3022@gmail.com>
2023-03-16 14:03:21 +0900

    feat: #5 Wishlist Entity 생성

---
+++ travel/src/main/java/com/travel/wishlist/entity/Wishlist.java
@@ -0,0 +1,29 @@
+package com.travel.wishlist.entity;
+
+import com.travel.member.entity.Member;
+import com.travel.product.entity.Product;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "wishlist")
+public class Wishlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "wishlist_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
