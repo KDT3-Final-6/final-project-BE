238bbb0afd5cbc50a0d0340f568a2d20f80e7c41
zoomin3022 <zoomin3022@gmail.com>
2023-03-18 20:33:43 +0900

    feat: #8 postPeriod 위한 DTO 생성

---
+++ travel/src/main/java/com/travel/product/dto/PeriodPostRequestDTO.java
@@ -0,0 +1,48 @@
+package com.travel.product.dto;
+
+import com.travel.product.entity.PeriodOption;
+import lombok.Getter;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Getter
+public class PeriodPostRequestDTO {
+    private Long productId;
+
+    @Valid
+    private List<PeriodOptionDTO> options = new ArrayList<>();
+
+    public List<PeriodOption> toEntities() {
+
+//        List<PeriodOption> periodOptionList = new ArrayList<>();
+//        for(PeriodOptionPostRequestDTO periodOption : periodOptions){
+//            periodOption.setDates();
+//            //request로 받은걸 파싱해서 Date에 다시 넣는용도
+//            periodOptionList.add(
+//                    PeriodOption.builder()
+//                            .startDate(periodOption.getStartDate())
+//                            .endDate(periodOption.getEndDate())
+//                            .maximumQuantity(periodOption.getMaximumQuantity())
+//                            .minimumQuantity(periodOption.getMinimumQuantity())
+//                            .build()
+//            );
+//        }
+
+        List<PeriodOption> periodOptionList = options.stream()
+                .peek(PeriodOptionDTO::setDates)
+                .map(periodOption -> PeriodOption.builder()
+                        .optionName(periodOption.getOptionName())
+                        .startDate(periodOption.getStartDate())
+                        .endDate(periodOption.getEndDate())
+                        .maximumQuantity(periodOption.getMaximumQuantity())
+                        .minimumQuantity(periodOption.getMinimumQuantity())
+                        .build())
+                .collect(toList());
+
+        return periodOptionList;
+    }
+}
