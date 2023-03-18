9ff49b28f641afb03cbd598a1aad5caa5793a51c
JeYeongR <wpdud2003@gmail.com>
2023-03-18 20:24:44 +0900

    feat: #9 Product를 PurchasedProduct로 바꾸는 메서드(toPurchase) 생성

--- travel/src/main/java/com/travel/product/entity/Product.java
+++ travel/src/main/java/com/travel/product/entity/Product.java
@@ -27,20 +27,18 @@ public class Product extends BaseEntity {
     @Column(name = "product_thumbnail")
     private String productThumbnail;
 
-    /*
-    @ElementCollection
-    @CollectionTable(name = "image")
-    private List<String> images = new ArrayList<>();
-    */
-
     @Column(name = "product_price")
     private Integer productPrice;
+
+    @Column(name = "product_category")
+    @Enumerated(EnumType.STRING)
+    private Category productCategory;
 
     @Column(name = "product_status")
     @Enumerated(EnumType.STRING)
     private Status productStatus;
 
-    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "product", cascade = CascadeType.ALL)
     private List<PeriodOption> periodOptions = new ArrayList<>();
 
     @OneToMany(mappedBy = "product")
@@ -67,5 +65,22 @@ public class Product extends BaseEntity {
         this.productStatus = productStatus;
         this.productContent = productContent;
         this.contentDetail = contentDetail;
+    }
+    /*
+    @ElementCollection
+    @CollectionTable(name = "image")
+    private List<String> images = new ArrayList<>();
+    */
+
+    public PurchasedProduct toPurchase(PeriodOption periodOption) {
+        return PurchasedProduct.builder()
+                .product(this)
+                .purchasedProductName(productName)
+                .purchasedProductThumbnail(productThumbnail)
+                .purchasedProductPrice(productPrice)
+                .startDate(periodOption.getStartDate())
+                .endDate(periodOption.getEndDate())
+                .period(periodOption.getPeriod())
+                .build();
     }
 }
