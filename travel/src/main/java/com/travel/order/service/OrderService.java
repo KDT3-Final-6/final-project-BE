dc48b6d1830237293acecd6f37e852581f755d88
JeYeongR <wpdud2003@gmail.com>
2023-03-18 20:15:12 +0900

    feat: #9 OrderService 생성

---
+++ travel/src/main/java/com/travel/order/service/OrderService.java
@@ -0,0 +1,6 @@
+package com.travel.order.service;
+
+public interface OrderService {
+
+
+}
---
+++ travel/src/main/java/com/travel/order/service/impl/OrderServiceImpl.java
@@ -0,0 +1,9 @@
+package com.travel.order.service.impl;
+
+import com.travel.order.service.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+}
