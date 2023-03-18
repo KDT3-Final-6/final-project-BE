804a7648fca7769a435c3455bafbd1948976e352
zoomin3022 <zoomin3022@gmail.com>
2023-03-18 20:33:43 +0900

    feat: #8 postPeriod 위한 DTO 생성

---
+++ travel/src/main/java/com/travel/product/dto/PeriodOptionDTO.java
@@ -0,0 +1,40 @@
+package com.travel.product.dto;
+
+import lombok.Getter;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+public class PeriodOptionDTO {
+
+    @NotBlank
+    private String optionName;
+
+    @NotBlank
+    private String periodString;
+
+    @Future
+    private LocalDate startDate;
+
+    @Future
+    private LocalDate endDate;
+
+    @Positive
+    private Integer maximumQuantity;
+
+    @Positive
+    private Integer minimumQuantity;
+
+    public void setDates() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+
+        String[] parts = periodString.split(" ");
+
+        startDate = LocalDate.parse(parts[0], formatter);
+        endDate = LocalDate.parse(parts[2], formatter);
+    }
+}
