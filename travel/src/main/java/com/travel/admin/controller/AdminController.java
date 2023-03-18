a40314c8bf2ab6fdc0daebb8c2110ed0f2572b9a
zoomin3022 <zoomin3022@gmail.com>
2023-03-18 20:35:34 +0900

    feat: #8 상품 추가, 상품 기간 옵션 추가 기능을 관리자의 AdminController에 구현

--- travel/src/main/java/com/travel/admin/controller/AdminController.java
+++ travel/src/main/java/com/travel/admin/controller/AdminController.java
@@ -1,4 +1,33 @@
 package com.travel.admin.controller;
 
+import com.travel.product.dto.PeriodPostRequestDTO;
+import com.travel.product.dto.ProductPostRequestDTO;
+import com.travel.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admins")
 public class AdminController {
+
+    private final ProductService productService;
+
+    @PostMapping("/products")
+    public ResponseEntity<String> postProduct(@RequestBody @Valid ProductPostRequestDTO productPostRequestDTO) {
+        productService.createProduct(productPostRequestDTO);
+        return ResponseEntity.ok(null);
+    }
+
+    @PostMapping("/products/periods")
+    public ResponseEntity postPeriod(@RequestBody @Valid PeriodPostRequestDTO periodPostRequestDTO) {
+        productService.createPeriodOptions(periodPostRequestDTO);
+        return ResponseEntity.ok(null);
+    }
 }
