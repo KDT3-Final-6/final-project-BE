983679d5ba24303564fd48d140d52213ae495d07
zoomin3022 <zoomin3022@gmail.com>
2023-03-18 20:29:45 +0900

    feat: #8 Category <-> Product 양방향 매핑 위한 중간 table ProductCategory 생성

---
+++ travel/src/main/java/com/travel/product/entity/ProductCategory.java
@@ -0,0 +1,34 @@
+package com.travel.product.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "product_category")
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_category_id")
+    private Long productCategoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public ProductCategory(Product product, Category category) {
+        this.product = product;
+        this.category = category;
+        product.getProductCategories().add(this);
+        category.getProductCategories().add(this);
+    }
+}
