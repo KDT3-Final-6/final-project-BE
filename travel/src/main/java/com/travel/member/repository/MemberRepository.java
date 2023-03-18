13dc09c8420f0caf4b77b36d5b9508bba47c647d
JeYeongR <wpdud2003@gmail.com>
2023-03-18 20:17:48 +0900

    feat: #9 MemberRepository 생성

---
+++ travel/src/main/java/com/travel/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.travel.member.repository;
+
+import com.travel.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
