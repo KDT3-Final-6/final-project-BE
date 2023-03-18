57a818222c9e4dce34f4456c409bf52e6bf50d44
JeYeongR <wpdud2003@gmail.com>
2023-03-18 20:13:25 +0900

    feat: #9 OrderRepository 생성

---
+++ travel/src/main/java/com/travel/order/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.travel.order.repository;
+
+import com.travel.member.entity.Member;
+import com.travel.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
