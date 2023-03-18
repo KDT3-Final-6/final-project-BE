fc8a27b743eff24c4378b98215b0e7e3c649b91d
zoomin3022 <zoomin3022@gmail.com>
2023-03-18 20:23:10 +0900

    feat: #8 enum Category -> Entity Category <-> Product 양방향 매핑

--- travel/src/main/java/com/travel/product/entity/Category.java
+++ travel/src/main/java/com/travel/product/entity/Category.java
@@ -1,12 +1,49 @@
 package com.travel.product.entity;
 
-public enum Category {
-    VACATION("휴양지"),
-    GOLF("골프");
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-    String name;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
-    Category(String name) {
-        this.name = name;
+import static javax.persistence.FetchType.LAZY;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @OneToMany(mappedBy = "category")
+    private List<ProductCategory> productCategories = new ArrayList<>();
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private CategoryEnum categoryEnum;
+
+    public void setParent(Category parent) {
+        this.parent = parent;
+    }
+
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
+
+    public Category(CategoryEnum categoryEnum) {
+        this.categoryEnum = categoryEnum;
     }
 }
