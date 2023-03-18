9b789e1ac607976dcc732d1fbd8b81d03b48ccf4
JeYeongR <wpdud2003@gmail.com>
2023-03-18 20:18:47 +0900

    feat: #9 MemberRepository에 findByMemberEmail 쿼리 메서드 생성

--- travel/src/main/java/com/travel/member/repository/MemberRepository.java
+++ travel/src/main/java/com/travel/member/repository/MemberRepository.java
@@ -4,7 +4,10 @@ import com.travel.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByMemberEmail(String email);
 }
