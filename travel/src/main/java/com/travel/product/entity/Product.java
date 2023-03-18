7d27abcc61bd66b48b36afc0329c4c8ea098dd3a
zoomin3022 <zoomin3022@gmail.com>
2023-03-18 20:31:55 +0900

    refactor: #8 PeriodOption, Product 연관관계 완성도 위해 리팩토링

--- travel/src/main/java/com/travel/product/entity/Product.java
+++ travel/src/main/java/com/travel/product/entity/Product.java
@@ -27,34 +27,45 @@ public class Product extends BaseEntity {
     @Column(name = "product_thumbnail")
     private String productThumbnail;
 
-    @Column(name = "product_price")
-    private Integer productPrice;
-
-    @Column(name = "product_category")
-    @Enumerated(EnumType.STRING)
-    private Category productCategory;
-
-    @Column(name = "product_status")
-    @Enumerated(EnumType.STRING)
-    private Status productStatus;
-
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "product", cascade = CascadeType.ALL)
-    private List<PeriodOption> periodOptions = new ArrayList<>();
-
-    @Column(name = "product_detail")
-    private String productDetail;
-
-    @Builder
-    public Product(String productName, String productThumbnail, Integer productPrice, Category productCategory, String productDetail) {
-        this.productName = productName;
-        this.productThumbnail = productThumbnail;
-        this.productPrice = productPrice;
-        this.productCategory = productCategory;
-        this.productDetail = productDetail;
-    }
     /*
     @ElementCollection
     @CollectionTable(name = "image")
     private List<String> images = new ArrayList<>();
     */
+
+    @Column(name = "product_price")
+    private Integer productPrice;
+
+    @Column(name = "product_status")
+    @Enumerated(EnumType.STRING)
+    private Status productStatus;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<PeriodOption> periodOptions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "product")
+    private List<ProductCategory> productCategories = new ArrayList<>();
+
+    //제목
+    @Column(name = "product_content")
+    private String productContent;
+
+    //상세
+    @Column(name = "content_detail")
+    private String contentDetail;
+
+    public void addPeriodOption(PeriodOption periodOption) {
+        periodOption.setProduct(this);
+        periodOptions.add(periodOption);
+    }
+
+    @Builder
+    public Product(String productName, String productThumbnail, Integer productPrice, Status productStatus, String productContent, String contentDetail) {
+        this.productName = productName;
+        this.productThumbnail = productThumbnail;
+        this.productPrice = productPrice;
+        this.productStatus = productStatus;
+        this.productContent = productContent;
+        this.contentDetail = contentDetail;
+    }
 }
