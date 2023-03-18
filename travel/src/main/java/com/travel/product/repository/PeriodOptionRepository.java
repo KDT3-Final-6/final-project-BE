e5798bc8a6feb468a87a412023e9d20355875041
JeYeongR <wpdud2003@gmail.com>
2023-03-18 20:23:08 +0900

    feat: #9 PeriodOptionRepository 생성

--- travel/src/main/java/com/travel/product/repository/PeriodOptionRepository.java
+++ travel/src/main/java/com/travel/product/repository/PeriodOptionRepository.java
@@ -2,7 +2,8 @@ package com.travel.product.repository;
 
 import com.travel.product.entity.PeriodOption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PeriodOptionRepository extends JpaRepository<PeriodOption, Long> {
-
 }
