a98e3f626f015adac812676d152bd33da2b3d1cf
JeYeongR <wpdud2003@gmail.com>
2023-03-19 14:50:55 +0900

    feat: #14 MethodArgumentNotValidException 예외 핸들러 생성

--- travel/src/main/java/com/travel/global/exception/CustomExceptionHandler.java
+++ travel/src/main/java/com/travel/global/exception/CustomExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.travel.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +15,11 @@ public class CustomExceptionHandler {
     public ResponseEntity<String> customExceptionHandler(CustomException e) {
         return ResponseEntity.status(e.getCustomExceptionType().getHttpStatus())
                 .body(e.getCustomExceptionType().getErrorMsg());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> methodArgumentNotValidExceptionHandler() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("잘못된 값입니다.");
     }
 }
