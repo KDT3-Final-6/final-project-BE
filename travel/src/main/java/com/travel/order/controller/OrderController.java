7ea3b247c4fc4876e55c9a0ba1b90df7ea9a9efd
JeYeongR <wpdud2003@gmail.com>
2023-03-19 21:07:44 +0900

    feat: #9 OrderController에 상품 주문 취소(deleteOrder) API 구현

--- travel/src/main/java/com/travel/order/controller/OrderController.java
+++ travel/src/main/java/com/travel/order/controller/OrderController.java
@@ -1,14 +1,12 @@
 package com.travel.order.controller;
 
 import com.travel.order.dto.request.OrderCreateListDTO;
-import com.travel.order.dto.response.OrderListResponseDTO;
 import com.travel.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +18,13 @@ public class OrderController {
     @PostMapping
     public ResponseEntity<Void> createOrder(@Valid @RequestBody OrderCreateListDTO orderCreateListDTO, String userEmail) {
         orderService.createOrder(orderCreateListDTO, userEmail);
+
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long orderId, String userEmail) {
+        orderService.deleteOrder(orderId, userEmail);
 
         return ResponseEntity.ok(null);
     }
