d509f770d24e77ac0966eb51d945eb3f604a1d12
JeYeongR <wpdud2003@gmail.com>
2023-03-19 20:56:49 +0900

    feat: #9 OrderController 리턴 수정 및 토큰 받기 전 임시로 userEmail 받게 만듦

--- travel/src/main/java/com/travel/order/controller/OrderController.java
+++ travel/src/main/java/com/travel/order/controller/OrderController.java
@@ -1,15 +1,14 @@
 package com.travel.order.controller;
 
 import com.travel.order.dto.request.OrderCreateListDTO;
+import com.travel.order.dto.response.OrderListResponseDTO;
 import com.travel.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,8 +18,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<String> createOrder(@Valid @RequestBody OrderCreateListDTO orderCreateListDTO) {
-        String userEmail = "test@test.com";
+    public ResponseEntity<Void> createOrder(@Valid @RequestBody OrderCreateListDTO orderCreateListDTO, String userEmail) {
         orderService.createOrder(orderCreateListDTO, userEmail);
 
         return ResponseEntity.ok(null);
