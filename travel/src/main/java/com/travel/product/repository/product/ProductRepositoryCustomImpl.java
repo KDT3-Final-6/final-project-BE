b331b3ec42e794dbfcce9d744a7f40e8462048ab
zoomin3022 <zoomin3022@gmail.com>
2023-03-20 17:16:04 +0900

    feat: #8 #18 ProductRepositoryCustomImpl 안에 findAllWithCheckBox 구현

---
+++ travel/src/main/java/com/travel/product/repository/product/ProductRepositoryCustomImpl.java
@@ -0,0 +1,44 @@
+package com.travel.product.repository.product;
+
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.travel.product.entity.Product;
+import com.travel.product.entity.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.travel.product.entity.QProduct.product;
+
+public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    public ProductRepositoryCustomImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public Page<Product> findAllWithCheckBox(Pageable pageable, Boolean includeSoldOut) {
+        QueryResults<Product> results = queryFactory
+                .select(product)
+                .from(product)
+                .where(productStatusRange(includeSoldOut))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+        List<Product> content = results.getResults();
+        long total = results.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    private Predicate productStatusRange(Boolean includeSoldOut) {
+        return includeSoldOut ? product.productStatus.in(Status.FORSALE, Status.SOLDOUT) : product.productStatus.eq(Status.FORSALE);
+    }
+}
