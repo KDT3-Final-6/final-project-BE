4135daa2ffd528446b381ec18558a57b7404ea5b
JeYeongR <wpdud2003@gmail.com>
2023-03-20 23:37:14 +0900

    feat: #9 상품 주문 내역 기능에 필요한 쿼리 메서드 생성

--- travel/src/main/java/com/travel/product/repository/PurchasedProductRepository.java
+++ travel/src/main/java/com/travel/product/repository/PurchasedProductRepository.java
@@ -1,7 +1,12 @@
 package com.travel.product.repository;
 
+import com.travel.order.entity.Order;
 import com.travel.product.entity.PurchasedProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PurchasedProductRepository extends JpaRepository<PurchasedProduct, Long> {
+
+    List<PurchasedProduct> findByOrder(Order order);
 }
