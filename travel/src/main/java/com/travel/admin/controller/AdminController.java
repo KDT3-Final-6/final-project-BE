183b2f6cf5207c495b87f68c553e0f3434db5d17
zoomin3022 <zoomin3022@gmail.com>
2023-03-20 15:30:27 +0900

    feat: #8 #17 GET /admins/products AdminController에 추가

--- travel/src/main/java/com/travel/admin/controller/AdminController.java
+++ travel/src/main/java/com/travel/admin/controller/AdminController.java
@@ -1,14 +1,13 @@
 package com.travel.admin.controller;
 
+import com.travel.global.response.PageResponseDTO;
 import com.travel.product.dto.request.PeriodPostRequestDTO;
 import com.travel.product.dto.request.ProductPostRequestDTO;
 import com.travel.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -17,12 +16,31 @@ import javax.validation.Valid;
 @RequestMapping("/admins")
 public class AdminController {
 
+    public static final int PAGE_SIZE = 1;
+
     private final ProductService productService;
 
     @PostMapping("/products")
     public ResponseEntity<String> postProduct(@RequestBody @Valid ProductPostRequestDTO productPostRequestDTO) {
         productService.createProduct(productPostRequestDTO);
         return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<PageResponseDTO> getProducts(@RequestParam(required = false, defaultValue = "1") String page) {
+
+        PageRequest pageRequest = null;
+
+        try {
+            int intPage = Integer.parseInt(page);
+            pageRequest = PageRequest.of(intPage - 1, PAGE_SIZE);
+            //정상적인 범위 내의 페이지 번호면 해당 페이지로
+        } catch (IllegalArgumentException e) {
+            pageRequest = PageRequest.of(0, PAGE_SIZE);
+            //음수나 오버플로 발생시키는 페이지 번호면 0번페이지로
+        }
+
+        return ResponseEntity.ok(productService.displayAdminProducts(pageRequest));
     }
 
     @PostMapping("/products/periods")
