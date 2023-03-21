e15c822f11cc5b29cc599bf0e8509b66f1f90961
zoomin3022 <zoomin3022@gmail.com>
2023-03-21 19:38:27 +0900

    feat: #24 CategoryEnum에 depth가 3 이상인 카테고리 예시 위해 하와이 추가

--- travel/src/main/java/com/travel/product/entity/CategoryEnum.java
+++ travel/src/main/java/com/travel/product/entity/CategoryEnum.java
@@ -6,7 +6,7 @@ public enum CategoryEnum {
     WITH_FAMILY("가족끼리"), ANYONE("누구든지"),
     LOCATION("지역별 여행"),
     AMERICA("아메리카"), ASIA("아시아"), AFRICA("아프리카"),
-    OCEANIA("오세아니아"), EUROPE("유럽"),
+    OCEANIA("오세아니아"), EUROPE("유럽"), HAWAII("하와이"),
     THEME("테마별 여행"),
     CULTURAL("문화탐방"), GOLF("골프여행"), VACATION("휴양지"),
     TREKKING("트레킹"), PILGRIMAGE("성지순례"), VOLUNTOUR("볼룬투어");
