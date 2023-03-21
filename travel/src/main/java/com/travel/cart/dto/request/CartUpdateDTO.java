c8f46a2f339a1bef6c4bd75e4df5a24684b50277
JeYeongR <wpdud2003@gmail.com>
2023-03-21 21:46:21 +0900

    feat: #23 장바구니 수정 리퀘스트 DTO 생성

---
+++ travel/src/main/java/com/travel/cart/dto/request/CartUpdateDTO.java
@@ -0,0 +1,20 @@
+package com.travel.cart.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class CartUpdateDTO {
+
+    @NotNull
+    private Long productId;
+
+    @NotNull
+    private Long periodOptionId;
+
+    @NotNull
+    private Integer quantity;
+}
