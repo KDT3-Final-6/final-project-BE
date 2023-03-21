27e0fb799ad861980826d1903dbeb198a3708982
JeYeongR <wpdud2003@gmail.com>
2023-03-21 20:20:38 +0900

    feat: #23 CartController에 장바구니 상품 추가(addCart) API 구현

--- travel/src/main/java/com/travel/cart/controller/CartController.java
+++ travel/src/main/java/com/travel/cart/controller/CartController.java
@@ -1,7 +1,27 @@
 package com.travel.cart.controller;
 
+import com.travel.cart.dto.CartAddDTO;
+import com.travel.cart.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/carts")
 public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping
+    public ResponseEntity<Void> addCart(@Valid @RequestBody CartAddDTO cartAddDTO, String userEmail) {
+
+        cartService.addCart(cartAddDTO, userEmail);
+        return ResponseEntity.ok(null);
+    }
 }
