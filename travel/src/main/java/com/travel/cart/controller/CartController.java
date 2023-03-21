626d53dd854a5de6ed1bafbe8652b6f5c164b2c1
JeYeongR <wpdud2003@gmail.com>
2023-03-21 21:29:30 +0900

    feat: #23 CartController에 장바구니 목록 조회(getCarts) API 구현

--- travel/src/main/java/com/travel/cart/controller/CartController.java
+++ travel/src/main/java/com/travel/cart/controller/CartController.java
@@ -3,7 +3,11 @@ package com.travel.cart.controller;
 import com.travel.cart.dto.request.CartAddDTO;
 import com.travel.cart.dto.request.CartDeleteListDTO;
 import com.travel.cart.service.CartService;
+import com.travel.global.exception.GlobalException;
+import com.travel.global.exception.GlobalExceptionType;
+import com.travel.global.response.PageResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +19,7 @@ import javax.validation.Valid;
 @RequestMapping("/carts")
 public class CartController {
 
+    public static final int PAGE_SIZE = 3;
     private final CartService cartService;
 
     @PostMapping
@@ -24,8 +29,22 @@ public class CartController {
         return ResponseEntity.ok(null);
     }
 
+    @GetMapping
+    public ResponseEntity<PageResponseDTO> getCarts(@RequestParam(required = false, defaultValue = "1") int page, String userEmail) {
+        if (page < 1) {
+            throw new GlobalException(GlobalExceptionType.PAGE_INDEX_NOT_POSITIVE_NUMBER);
+        }
+
+        PageRequest pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
+
+        PageResponseDTO carts = cartService.getCarts(pageRequest, userEmail);
+
+        return ResponseEntity.ok(carts);
+    }
+
     @DeleteMapping
-    public ResponseEntity<Void> deleteCarts(@Valid @RequestBody CartDeleteListDTO cartDeleteListDTO, String userEmail) {
+    public ResponseEntity<Void> deleteCarts(@Valid @RequestBody CartDeleteListDTO cartDeleteListDTO, String
+            userEmail) {
         cartService.deleteCarts(cartDeleteListDTO, userEmail);
 
         return ResponseEntity.ok(null);
