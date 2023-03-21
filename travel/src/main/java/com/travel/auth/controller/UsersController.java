063863dd1a86e2ab03d87b3b488fa8fdf4df77e3
lala9663 <lala96632040@gmail.com>
2023-03-21 21:51:33 +0900

    feat: UsersController
    회원관련 Controller

---
+++ travel/src/main/java/com/travel/auth/controller/UsersController.java
@@ -0,0 +1,79 @@
+package com.travel.auth.controller;
+
+
+
+import com.travel.auth.dto.Response;
+import com.travel.auth.dto.UserRequestDto;
+import com.travel.auth.jwt.JwtTokenProvider;
+import com.travel.auth.lib.Helper;
+import com.travel.auth.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class UsersController {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UsersService usersService;
+    private final Response response;
+
+    @PostMapping("/members")
+    public ResponseEntity<?> signUp(@Validated @RequestBody UserRequestDto.SignUp signUp, Errors errors) {
+        // validation check
+        if (errors.hasErrors()) {
+            return response.invalidFields(Helper.refineErrors(errors));
+        }
+        return usersService.signUp(signUp);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Validated @RequestBody UserRequestDto.Login login, Errors errors) {
+        // validation check
+        if (errors.hasErrors()) {
+            return response.invalidFields(Helper.refineErrors(errors));
+        }
+        return usersService.login(login);
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(@Validated UserRequestDto.Reissue reissue, Errors errors) {
+        // validation check
+        if (errors.hasErrors()) {
+            return response.invalidFields(Helper.refineErrors(errors));
+        }
+        return usersService.reissue(reissue);
+    }
+
+    @PostMapping("/members/logout")
+    public ResponseEntity<?> logout(@Validated UserRequestDto.Logout logout, Errors errors) {
+        // validation check
+        if (errors.hasErrors()) {
+            return response.invalidFields(Helper.refineErrors(errors));
+        }
+        return usersService.logout(logout);
+    }
+
+    @GetMapping("/authority")
+    public ResponseEntity<?> authority() {
+        log.info("ADD ROLE_ADMIN");
+        return usersService.authority();
+    }
+
+    @GetMapping("/userTest")
+    public ResponseEntity<?> userTest() {
+        log.info("ROLE_USER TEST");
+        return response.success();
+    }
+
+    @GetMapping("/adminTest")
+    public ResponseEntity<?> adminTest() {
+        log.info("ROLE_ADMIN TEST");
+        return response.success();
+    }
+}
