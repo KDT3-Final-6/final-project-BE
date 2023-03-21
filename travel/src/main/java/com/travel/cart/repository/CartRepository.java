29789c189d97eea903ec2e18dc64232bbde7f0ba
JeYeongR <wpdud2003@gmail.com>
2023-03-21 21:27:41 +0900

    feat: #23 CartRepository에 쿼리 메서드 추가

--- travel/src/main/java/com/travel/cart/repository/CartRepository.java
+++ travel/src/main/java/com/travel/cart/repository/CartRepository.java
@@ -5,10 +5,13 @@ import com.travel.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     Optional<Cart> findByMemberAndCartId(Member member, Long cartId);
+
+    List<Cart> findByMember(Member member);
 }
