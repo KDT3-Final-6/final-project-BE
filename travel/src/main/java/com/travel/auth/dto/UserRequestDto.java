bf1d5f913902211225cd7bf31e9ff2fbd2b937b1
lala9663 <lala96632040@gmail.com>
2023-03-21 21:50:48 +0900

    feat: dto
    UserResponseDto: 토큰관련
    UserRequestDto: 회원가입, 로그인, 토큰, 로그아웃
    Response: 응답 코드

--- travel/src/main/java/com/travel/auth/dto/UserRequestDto.java
+++ travel/src/main/java/com/travel/auth/dto/UserRequestDto.java
@@ -52,4 +52,24 @@ public class UserRequestDto {
         }
     }
 
+    @Getter
+    @Setter
+    public static class Reissue {
+        @NotEmpty(message = "accessToken 을 입력해주세요.")
+        private String accessToken;
+
+        @NotEmpty(message = "refreshToken 을 입력해주세요.")
+        private String refreshToken;
+    }
+
+    @Getter
+    @Setter
+    public static class Logout {
+        @NotEmpty(message = "잘못된 요청입니다.")
+        private String accessToken;
+
+        @NotEmpty(message = "잘못된 요청입니다.")
+        private String refreshToken;
+    }
+
 }
