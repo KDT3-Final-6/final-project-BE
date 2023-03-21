864a6e1495d8202a3db28aea018076236673a5ea
JeYeongR <wpdud2003@gmail.com>
2023-03-21 20:59:12 +0900

    feat: #23 CartController에 장바구니 상품 삭제(deleteCarts) API 구현

--- travel/src/main/java/com/travel/cart/controller/CartController.java
+++ travel/src/main/java/com/travel/cart/controller/CartController.java
@@ -6,9 +6,7 @@ import com.travel.cart.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -21,8 +19,15 @@ public class CartController {
 
     @PostMapping
     public ResponseEntity<Void> addCart(@Valid @RequestBody CartAddDTO cartAddDTO, String userEmail) {
-
         cartService.addCart(cartAddDTO, userEmail);
+
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteCarts(@Valid @RequestBody CartDeleteListDTO cartDeleteListDTO, String userEmail) {
+        cartService.deleteCarts(cartDeleteListDTO, userEmail);
+
         return ResponseEntity.ok(null);
     }
 }
