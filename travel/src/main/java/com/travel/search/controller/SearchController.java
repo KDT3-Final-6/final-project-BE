491fe2aa27e2086dbb5ac7a9642d0859134b58a3
zoomin3022 <zoomin3022@gmail.com>
2023-03-21 20:11:27 +0900

    feat: #24 GET /search/category SearchController에 구현

--- travel/src/main/java/com/travel/search/controller/SearchController.java
+++ travel/src/main/java/com/travel/search/controller/SearchController.java
@@ -1,4 +1,35 @@
 package com.travel.search.controller;
 
+import com.travel.global.exception.GlobalException;
+import com.travel.global.exception.GlobalExceptionType;
+import com.travel.product.dto.response.ProductCategoryToProductPage;
+import com.travel.search.service.SearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.travel.admin.controller.AdminController.PAGE_SIZE;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/search")
 public class SearchController {
+
+    private final SearchService searchService;
+
+    @GetMapping("/category")
+    public ResponseEntity<ProductCategoryToProductPage> getProductsByCategory(
+            @RequestParam String category, @RequestParam(required = false, defaultValue = "1") int page) {
+
+        if (page < 1) {
+            throw new GlobalException(GlobalExceptionType.PAGE_INDEX_NOT_POSITIVE_NUMBER);
+        }
+        PageRequest pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
+
+        return ResponseEntity.ok(searchService.displayProductsByCategory(pageRequest, category));
+    }
 }
