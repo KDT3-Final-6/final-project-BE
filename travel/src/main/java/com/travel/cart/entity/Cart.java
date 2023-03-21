7350a3954077dda61e8a38a7b1803e8c81d33971
JeYeongR <wpdud2003@gmail.com>
2023-03-21 20:18:14 +0900

    feat: #23 Cart 엔티티 수정

--- travel/src/main/java/com/travel/cart/entity/Cart.java
+++ travel/src/main/java/com/travel/cart/entity/Cart.java
@@ -2,8 +2,10 @@ package com.travel.cart.entity;
 
 import com.travel.global.entity.BaseEntity;
 import com.travel.member.entity.Member;
+import com.travel.product.entity.PeriodOption;
 import com.travel.product.entity.Product;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -27,4 +29,19 @@ public class Cart extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "period_option_id")
+    private PeriodOption periodOption;
+
+    @Column(name = "cart_quantity")
+    private Integer cartQuantity;
+
+    @Builder
+    public Cart(Member member, Product product, PeriodOption periodOption, Integer cartQuantity) {
+        this.member = member;
+        this.product = product;
+        this.periodOption = periodOption;
+        this.cartQuantity = cartQuantity;
+    }
 }
