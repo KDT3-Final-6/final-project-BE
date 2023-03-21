ac5885e640089dc48bc59544213d3027d9755bba
lala9663 <lala96632040@gmail.com>
2023-03-21 16:43:28 +0900

    feat: UserRequestDto 회원가입 기능

---
+++ travel/src/main/java/com/travel/auth/dto/UserRequestDto.java
@@ -0,0 +1,54 @@
+package com.travel.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class UserRequestDto {
+
+    @Getter
+    @Setter
+    public static class SignUp {
+        @NotEmpty(message = "이메일은 필수 입력값입니다.")
+        @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
+        private String email;
+
+        @NotEmpty(message = "비밀번호는 필수 입력값입니다.")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+        private String password;
+
+        @NotEmpty(message = "아이디를 적어주세요")
+        @Size(min = 2, max = 10, message = "2글자 이상 10글자 이하로 입력해주세요")
+        private String nickname;
+
+        @NotEmpty(message = "휴대폰 번호를 작성해주세요")
+        @Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$", message = "000-0000-0000 으로 작성해주세요")
+        private String phoneNumber;
+
+        @NotEmpty(message = "생일을 적어주세요")
+        @Pattern(regexp = "^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$", message = "YYYY-MM-DD 으로 작성해주세요")
+        private String birthDate;
+
+        @NotEmpty(message = "취미를 적어주세요")
+        private String hobby;
+    }
+
+    @Getter
+    @Setter
+    public static class Login {
+        @NotEmpty(message = "이메일은 필수 입력값입니다.")
+        @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
+        private String email;
+
+        @NotEmpty(message = "비밀번호는 필수 입력값입니다.")
+        private String password;
+
+        public UsernamePasswordAuthenticationToken toAuthentication() {
+            return new UsernamePasswordAuthenticationToken(email, password);
+        }
+    }
+}
