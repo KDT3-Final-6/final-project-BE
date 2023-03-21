26c9f8d217a4c3a11655a9ac59eaa0b093eb3eb8
zoomin3022 <zoomin3022@gmail.com>
2023-03-21 20:08:14 +0900

    feat: #24 ProductDetailGetResponseDTO에서 Category 반환 로직을 계층적 구조로 변경

--- travel/src/main/java/com/travel/product/dto/response/ProductDetailGetResponseDTO.java
+++ travel/src/main/java/com/travel/product/dto/response/ProductDetailGetResponseDTO.java
@@ -1,5 +1,6 @@
 package com.travel.product.dto.response;
 
+import com.travel.product.entity.Category;
 import com.travel.product.entity.PeriodOption;
 import com.travel.product.entity.Product;
 import com.travel.product.entity.ProductCategory;
@@ -19,7 +20,7 @@ public class ProductDetailGetResponseDTO {
     private String contentDetail;
 
     private List<PeriodOption> periodOptions = new ArrayList<>();
-    private List<String> productCategories = new ArrayList<>();
+    private List<StringBuffer> productCategories = new ArrayList<>();
 
     public ProductDetailGetResponseDTO(Product product) {
         this.productName = product.getProductName();
@@ -29,8 +30,18 @@ public class ProductDetailGetResponseDTO {
         this.productContent = product.getProductContent();
         this.contentDetail = product.getContentDetail();
         this.periodOptions.addAll(product.getPeriodOptions());
+
         for (ProductCategory productCategory : product.getProductCategories()) {
-            this.productCategories.add(productCategory.getCategory().getCategoryEnum().getKorean());
+            if (productCategory.getCategory().getChild().size() == 0) {
+                StringBuffer stringBuffer = new StringBuffer();
+                stringBuffer.append(productCategory.getCategory().getCategoryEnum().getKorean());
+                Category parent = productCategory.getCategory().getParent();
+                while (parent != null) {
+                    stringBuffer.insert(0, ": ").insert(0, parent.getCategoryEnum().getKorean());
+                    parent = parent.getParent();
+                }
+                productCategories.add(stringBuffer);
+            }
         }
     }
 }
