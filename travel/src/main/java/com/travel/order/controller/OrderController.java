629c14036e5fa1c86c0e48d4f27b30583fb455c0
JeYeongR <wpdud2003@gmail.com>
2023-03-21 12:29:39 +0900

    feat: #9 OrderController에 상품 주문 내역(getOrders) API 구현

--- travel/src/main/java/com/travel/order/controller/OrderController.java
+++ travel/src/main/java/com/travel/order/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.travel.order.controller;
 
+import com.travel.global.response.PageResponseDTO;
 import com.travel.order.dto.request.OrderCreateListDTO;
 import com.travel.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,8 @@ import javax.validation.Valid;
 @RequestMapping("/orders")
 public class OrderController {
 
+    public static final int PAGE_SIZE = 3;
+
     private final OrderService orderService;
 
     @PostMapping
@@ -20,6 +24,23 @@ public class OrderController {
         orderService.createOrder(orderCreateListDTO, userEmail);
 
         return ResponseEntity.ok(null);
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponseDTO> getOrders(@RequestParam(required = false, defaultValue = "1") int page, String userEmail) {
+        PageRequest pageRequest;
+
+        try {
+            pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
+            //정상적인 범위 내의 페이지 번호면 해당 페이지로
+        } catch (IllegalArgumentException e) {
+            pageRequest = PageRequest.of(0, PAGE_SIZE);
+            //음수나 오버플로 발생시키는 페이지 번호면 0번페이지로
+        }
+
+        PageResponseDTO orders = orderService.getOrders(pageRequest, userEmail);
+
+        return ResponseEntity.ok(orders);
     }
 
     @DeleteMapping("/{orderId}")
