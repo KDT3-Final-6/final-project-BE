525833094a9250903f970899a3621099a29ce432
zoomin3022 <zoomin3022@gmail.com>
2023-03-22 14:36:30 +0900

    feat: #28 PATCH용 ProductPatchRequestDTO 생성

---
+++ travel/src/main/java/com/travel/product/dto/request/ProductPatchRequestDTO.java
@@ -0,0 +1,48 @@
+package com.travel.product.dto.request;
+
+import com.travel.product.entity.Product;
+import com.travel.product.entity.Status;
+import lombok.Getter;
+
+import javax.validation.constraints.Positive;
+import java.util.Optional;
+
+@Getter
+public class ProductPatchRequestDTO {
+
+    private String productName;
+    private String productThumbnail;
+    private String productStatus;
+    private String productContent;
+    private String contentDetail;
+
+    @Positive
+    private Integer productPrice;
+
+    public Status setEnumProductStatus(String productStatus) {
+        if (productStatus.equals(Status.FORSALE.getKorean())) {
+            return Status.FORSALE;
+        } else if (productStatus.equals(Status.SOLDOUT.getKorean())) {
+            return Status.SOLDOUT;
+        }
+        return Status.HIDDEN;
+    }
+
+    public Product toEntity(Product product) {
+        productName = Optional.ofNullable(productName).orElse(product.getProductName());
+        productThumbnail = Optional.ofNullable(productThumbnail).orElse(product.getProductThumbnail());
+        productStatus = Optional.ofNullable(productStatus).orElse(product.getProductStatus().getKorean());
+        productContent = Optional.ofNullable(productContent).orElse(product.getProductContent());
+        contentDetail = Optional.ofNullable(contentDetail).orElse(product.getContentDetail());
+        productPrice=Optional.ofNullable(productPrice).orElse(product.getProductPrice());
+
+        return Product.builder()
+                .productName(productName)
+                .productThumbnail(productThumbnail)
+                .productPrice(productPrice)
+                .productStatus(setEnumProductStatus(productStatus))
+                .productContent(productContent)
+                .contentDetail(contentDetail)
+                .build();
+    }
+}
