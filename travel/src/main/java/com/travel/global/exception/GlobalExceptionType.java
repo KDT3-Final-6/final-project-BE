ea22fbd31ae2b33a793168fefc027179846dc14d
JeYeongR <wpdud2003@gmail.com>
2023-03-23 16:45:58 +0900

    feat: #29 글로벌 커스텀 예외 추가

--- travel/src/main/java/com/travel/global/exception/GlobalExceptionType.java
+++ travel/src/main/java/com/travel/global/exception/GlobalExceptionType.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum GlobalExceptionType implements CustomExceptionType {
 
-    PAGE_INDEX_NOT_POSITIVE_NUMBER(HttpStatus.BAD_REQUEST, "Page는 0보다 커야합니다.");
+    PAGE_INDEX_NOT_POSITIVE_NUMBER(HttpStatus.BAD_REQUEST, "Page는 0보다 커야합니다."),
+    WRONG_DATE_FORMAT(HttpStatus.BAD_REQUEST, "날짜 형식은 yyyy-MM-dd 입니다.");
     private final HttpStatus httpStatus;
     private final String errorMsg;
 
