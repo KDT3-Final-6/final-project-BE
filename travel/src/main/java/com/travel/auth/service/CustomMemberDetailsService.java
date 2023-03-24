7ee628ae0e24133b29df7372a92becd39e6dd77f
lala9663 <lala96632040@gmail.com>
2023-03-24 14:16:19 +0900

    refactoring: user 패키지에서 member 패키지로 변경

---
+++ travel/src/main/java/com/travel/auth/service/CustomMemberDetailsService.java
@@ -0,0 +1,32 @@
+package com.travel.auth.service;
+
+import com.travel.member.repository.MemberRepository;
+import com.travel.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+@RequiredArgsConstructor
+public class CustomMemberDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByMemberEmail(username)
+                .map(this::createMemberDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
+    }
+
+    // 해당하는 Member 의 데이터가 존재한다면 MemberDetails 객체로 만들어서 리턴
+    private UserDetails createMemberDetails(Member member) {
+        return new User(member.getUsername(), member.getPassword(), member.getAuthorities());
+    }
+}
