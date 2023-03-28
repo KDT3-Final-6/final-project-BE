84fa6854f6fec0a08c1753f9ed9f435f5701b9e0
JeYeongR <wpdud2003@gmail.com>
2023-03-29 01:20:03 +0900

    feat: #64 WishlistService 생성

---
+++ travel/src/main/java/com/travel/wishlist/service/WishlistService.java
@@ -0,0 +1,4 @@
+package com.travel.wishlist.service;
+
+public interface WishlistService {
+}
---
+++ travel/src/main/java/com/travel/wishlist/service/impl/WishlistServiceImpl.java
@@ -0,0 +1,8 @@
+package com.travel.wishlist.service.impl;
+
+import com.travel.wishlist.service.WishlistService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WishlistServiceImpl implements WishlistService {
+}
