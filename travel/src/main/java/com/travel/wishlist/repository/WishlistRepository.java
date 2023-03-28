aedd0895b86990244f8c8dfa97342a7309157402
JeYeongR <wpdud2003@gmail.com>
2023-03-29 01:19:47 +0900

    feat: #64 WishlistRepository 생성

---
+++ travel/src/main/java/com/travel/wishlist/repository/WishlistRepository.java
@@ -0,0 +1,9 @@
+package com.travel.wishlist.repository;
+
+import com.travel.wishlist.entity.Wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+}
