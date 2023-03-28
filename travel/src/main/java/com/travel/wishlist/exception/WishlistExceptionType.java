c0d59597dbc3be90733056fbc8220afd52f64f57
JeYeongR <wpdud2003@gmail.com>
2023-03-29 02:09:17 +0900

    feat: #64 Wishlist 커스텀 예외 추가

--- travel/src/main/java/com/travel/wishlist/exception/WishlistExceptionType.java
+++ travel/src/main/java/com/travel/wishlist/exception/WishlistExceptionType.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum WishlistExceptionType implements CustomExceptionType {
 
-    ALREADY_IN_WISHLIST(HttpStatus.CONFLICT, "위시리스트에 이미 존재하는 상품입니다.");
+    ALREADY_IN_WISHLIST(HttpStatus.CONFLICT, "위시리스트에 이미 존재하는 상품입니다."),
+    WISHLIST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 위시리스트입니다.");
 
     private final HttpStatus httpStatus;
     private final String errorMsg;
