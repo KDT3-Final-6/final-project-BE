08097eea732983eeae3b9156aac122e62ba9c340
lala9663 <lala96632040@gmail.com>
2023-03-28 16:26:36 +0900

    feat: controller 회원 정보 조회 API 구현

--- travel/src/main/java/com/travel/member/controller/MemberController.java
+++ travel/src/main/java/com/travel/member/controller/MemberController.java
@@ -1,4 +1,60 @@
 package com.travel.member.controller;
 
+import com.travel.auth.jwt.JwtTokenProvider;
+import com.travel.member.dto.responseDTO.MemberResponseDTO;
+import com.travel.member.entity.Member;
+import com.travel.member.service.MemberService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
 public class MemberController {
+
+    private final MemberService memberService;
+    private final JwtTokenProvider tokenProvider;
+
+    // 회원정보
+    @GetMapping("/members")
+    public ResponseEntity<MemberResponseDTO.MemberInfoResponseDTO> getMyMemberInfo(@RequestHeader("Authorization") String authorizationHeader) {
+        // "Bearer " 이후의 토큰 값 추출
+        String token = authorizationHeader.substring(7);
+
+        // JWT 파싱하여 회원 아이디 추출
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(tokenProvider.getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        String memberEmail = (claims.getSubject());
+
+
+        // 회원 아이디를 토대로 회원 정보 조회
+        Member member = memberService.getMemberByMemberEmail(memberEmail);
+
+        // 회원 정보가 없는 경우 404 응답 반환
+        if (member == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // 회원 정보를 DTO 객체에 매핑
+        MemberResponseDTO.MemberInfoResponseDTO memberInfo = new MemberResponseDTO.MemberInfoResponseDTO();
+        memberInfo.setMemberEmail(member.getMemberEmail());
+        memberInfo.setMemberName(member.getMemberName());
+        memberInfo.setMemberNickName(member.getMemberNickname());
+        memberInfo.setMemberPhone(member.getMemberPhone());
+        memberInfo.setMemberBirthDate(member.getMemberBirthDate());
+        memberInfo.setMemberHobby(member.getMemberHobby().toString());
+        memberInfo.setGrade(member.getMemberGrade());
+        // 회원 정보를 담은 DTO 객체와 200 응답 반환
+        return ResponseEntity.ok(memberInfo);
+    }
+
 }
+
