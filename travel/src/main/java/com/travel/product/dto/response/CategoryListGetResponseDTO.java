aa3a8e00bf32fe42630f3d5b153d93ddc99ecbc3
zoomin3022 <zoomin3022@gmail.com>
2023-03-28 19:55:04 +0900

    feat: #28 카테고리 리스트 GET 위한 DTO 생성

---
+++ travel/src/main/java/com/travel/product/dto/response/CategoryListGetResponseDTO.java
@@ -0,0 +1,30 @@
+package com.travel.product.dto.response;
+
+import com.travel.product.entity.Category;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class CategoryListGetResponseDTO {
+    private Long categoryId;
+    private String categoryName;
+    private List<CategoryListGetResponseDTO> child = new ArrayList<>();
+
+    public static CategoryListGetResponseDTO of(Category category) {
+        return new CategoryListGetResponseDTO(
+                category.getCategoryId(),
+                category.getCategoryName(),
+                category.getChild().stream().map(CategoryListGetResponseDTO::of).collect(Collectors.toList())
+        );
+    }
+
+
+    public CategoryListGetResponseDTO(Long categoryId, String categoryName, List<CategoryListGetResponseDTO> children) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.child = children;
+    }
+}
