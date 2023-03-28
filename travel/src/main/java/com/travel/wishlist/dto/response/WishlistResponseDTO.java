eb223cb1ba39f57a8edcd19d64999832519bcbea
JeYeongR <wpdud2003@gmail.com>
2023-03-29 01:52:11 +0900

    feat: #64 WishlistResponseDTO 생성

---
+++ travel/src/main/java/com/travel/wishlist/dto/response/WishlistResponseDTO.java
@@ -0,0 +1,17 @@
+package com.travel.wishlist.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class WishlistResponseDTO {
+
+    private Long productId;
+
+    private String productName;
+
+    private String productThumbnail;
+
+    private Integer productPrice;
+}
