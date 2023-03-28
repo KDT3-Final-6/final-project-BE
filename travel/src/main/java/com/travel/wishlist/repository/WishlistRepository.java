566c30778d263ff330c0fd78bf1f129160ee2d7d
JeYeongR <wpdud2003@gmail.com>
2023-03-29 01:37:39 +0900

    feat: #64 WishlistRepository에 쿼리메서드 추가

--- travel/src/main/java/com/travel/wishlist/repository/WishlistRepository.java
+++ travel/src/main/java/com/travel/wishlist/repository/WishlistRepository.java
@@ -1,9 +1,13 @@
 package com.travel.wishlist.repository;
 
+import com.travel.member.entity.Member;
+import com.travel.product.entity.Product;
 import com.travel.wishlist.entity.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+
+    boolean existsByMemberAndProduct(Member member, Product product);
 }
