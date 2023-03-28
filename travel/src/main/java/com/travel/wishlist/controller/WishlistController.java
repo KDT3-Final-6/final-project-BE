242341115781318b15c11c454d63d8255eb262b0
JeYeongR <wpdud2003@gmail.com>
2023-03-29 01:52:59 +0900

    feat: #64 WishlistController에 위시리스트 목록 조회(getWishlists) API 구현

--- travel/src/main/java/com/travel/wishlist/controller/WishlistController.java
+++ travel/src/main/java/com/travel/wishlist/controller/WishlistController.java
@@ -1,17 +1,20 @@
 package com.travel.wishlist.controller;
 
+import com.travel.global.exception.GlobalException;
+import com.travel.global.exception.GlobalExceptionType;
+import com.travel.global.response.PageResponseDTO;
 import com.travel.wishlist.service.WishlistService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/wishlist")
 public class WishlistController {
+
+    public static final int PAGE_SIZE = 3;
 
     private final WishlistService wishlistService;
 
@@ -20,6 +23,19 @@ public class WishlistController {
         wishlistService.addWishlist(productId, userEmail);
 
         return ResponseEntity.ok(null);
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponseDTO> getWishlists(@RequestParam(required = false, defaultValue = "1") int page, String userEmail) {
+        if (page < 1) {
+            throw new GlobalException(GlobalExceptionType.PAGE_INDEX_NOT_POSITIVE_NUMBER);
+        }
+
+        PageRequest pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
+
+        PageResponseDTO wishlists = wishlistService.getWishlists(pageRequest, userEmail);
+
+        return ResponseEntity.ok(wishlists);
     }
 
 }
