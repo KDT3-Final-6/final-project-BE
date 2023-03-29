0d816101018b2d3ac3dd6327e51b82a3946441ee
zoomin3022 <zoomin3022@gmail.com>
2023-03-29 19:43:21 +0900

    feat: #72 MemberImageRepository 에서 Member로 삭제하는 메소드 추가

--- travel/src/main/java/com/travel/image/repository/MemberImageRepository.java
+++ travel/src/main/java/com/travel/image/repository/MemberImageRepository.java
@@ -1,7 +1,10 @@
 package com.travel.image.repository;
 
 import com.travel.image.entity.MemberImage;
+import com.travel.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberImageRepository extends JpaRepository<MemberImage, Long> {
+
+    void deleteByMember(Member member);
 }
