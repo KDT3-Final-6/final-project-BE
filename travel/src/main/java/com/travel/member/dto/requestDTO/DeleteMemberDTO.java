1dd4766ed8db16b261fcbfafa8047015df351b43
lala9663 <lala96632040@gmail.com>
2023-03-31 03:21:32 +0900

    refactoring: DeleteMember DTO
    회원 삭제 DTO 리펙토링

--- travel/src/main/java/com/travel/member/dto/requestDTO/DeleteMemberDTO.java
+++ travel/src/main/java/com/travel/member/dto/requestDTO/DeleteMemberDTO.java
@@ -8,24 +8,8 @@ import lombok.ToString;
 @Setter
 @ToString
 public class DeleteMemberDTO {
-    private String memberEmail;
     private String memberPassword;
 
-    public DeleteMemberDTO() {
-    }
-
-    public DeleteMemberDTO(String memberEmail, String memberPassword) {
-        this.memberEmail = memberEmail;
-        this.memberPassword = memberPassword;
-    }
-
-    public String getMemberEmail() {
-        return memberEmail;
-    }
-
-    public void setMemberEmail(String memberEmail) {
-        this.memberEmail = memberEmail;
-    }
 
     public String getMemberPassword() {
         return memberPassword;
