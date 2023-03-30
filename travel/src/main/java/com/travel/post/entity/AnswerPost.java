ace1a96a157c8c1944aaa51dcdae0f7c708f6ba6
JeYeongR <wpdud2003@gmail.com>
2023-03-31 02:43:34 +0900

    feat: #92 QnAAnswerRequestDTO 생성

--- travel/src/main/java/com/travel/post/entity/AnswerPost.java
+++ travel/src/main/java/com/travel/post/entity/AnswerPost.java
@@ -2,6 +2,7 @@ package com.travel.post.entity;
 
 import com.travel.product.entity.PurchasedProduct;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,7 +19,16 @@ public class AnswerPost {
     @Column(name = "answer_id")
     private Long answerId;
 
+    @Column(name = "answer_content")
+    private String answerContent;
+
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private QnAPost qnAPost;
+
+    @Builder
+    public AnswerPost(String answerContent, QnAPost qnAPost) {
+        this.answerContent = answerContent;
+        this.qnAPost = qnAPost;
+    }
 }
