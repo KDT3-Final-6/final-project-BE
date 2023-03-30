de27434fc16a340c4421c990a6176bcee65263b2
JeYeongR <wpdud2003@gmail.com>
2023-03-31 01:58:17 +0900

    feat: #92 빌더패턴으로 DTO로 만드는 메서드 생성

--- travel/src/main/java/com/travel/post/entity/QnAProductPost.java
+++ travel/src/main/java/com/travel/post/entity/QnAProductPost.java
@@ -1,6 +1,8 @@
 package com.travel.post.entity;
 
 
+import com.travel.post.dto.response.QnAAdminResponseDTO;
+import com.travel.post.dto.response.QnAResponseDTO;
 import com.travel.product.entity.PurchasedProduct;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -23,4 +25,29 @@ public class QnAProductPost extends QnAPost {
         super(qnAPost.getPostTitle(), qnAPost.getPostContent(), qnAPost.getMember(), qnAPost.getInquiryType());
         this.purchasedProduct = purchasedProduct;
     }
+
+    public QnAResponseDTO toQnAResponseDTO() {
+        return QnAResponseDTO.builder()
+                .postId(this.getPostId())
+                .postTitle(this.getPostTitle())
+                .postContent(this.getPostContent())
+                .inquiryType(this.getInquiryType().getKorean())
+                .qnAStatus(this.getQnAStatus().getKorean())
+                .purchasedProductName(this.getPurchasedProduct().getPurchasedProductName())
+                .createdDate(this.getCreatedDate())
+                .build();
+    }
+
+    public QnAAdminResponseDTO toQnAAdminResponseDTO() {
+            return QnAAdminResponseDTO.builder()
+                    .postId(this.getPostId())
+                    .postTitle(this.getPostTitle())
+                    .postContent(this.getPostContent())
+                    .inquiryType(this.getInquiryType().getKorean())
+                    .qnAStatus(this.getQnAStatus().getKorean())
+                    .purchasedProductName(this.getPurchasedProduct().getPurchasedProductName())
+                    .createdDate(this.getCreatedDate())
+                    .memberName(this.getMember().getMemberName())
+                    .build();
+        }
 }
