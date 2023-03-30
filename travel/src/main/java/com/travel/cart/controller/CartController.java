bcf9766c35887470ca0f9bee4ab3f80d7b741b24
JeYeongR <wpdud2003@gmail.com>
2023-03-30 23:46:52 +0900

    feat: #81 컨트롤러에서 토큰을 받게 수정

--- travel/src/main/java/com/travel/cart/controller/CartController.java
+++ travel/src/main/java/com/travel/cart/controller/CartController.java
@@ -10,6 +10,7 @@ import com.travel.global.response.PageResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,36 +25,34 @@ public class CartController {
     private final CartService cartService;
 
     @PostMapping
-    public ResponseEntity<Void> addCart(@Valid @RequestBody CartAddListDTO cartAddListDTO, String userEmail) {
-        cartService.addCart(cartAddListDTO, userEmail);
+    public ResponseEntity<Void> addCart(@Valid @RequestBody CartAddListDTO cartAddListDTO, Authentication authentication) {
+        cartService.addCart(cartAddListDTO, authentication.getName());
 
         return ResponseEntity.ok(null);
     }
 
     @GetMapping
-    public ResponseEntity<PageResponseDTO> getCarts(@RequestParam(required = false, defaultValue = "1") int page, String userEmail) {
+    public ResponseEntity<PageResponseDTO> getCarts(@RequestParam(required = false, defaultValue = "1") int page, Authentication authentication) {
         if (page < 1) {
             throw new GlobalException(GlobalExceptionType.PAGE_INDEX_NOT_POSITIVE_NUMBER);
         }
 
         PageRequest pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
-
-        PageResponseDTO carts = cartService.getCarts(pageRequest, userEmail);
+        PageResponseDTO carts = cartService.getCarts(pageRequest, authentication.getName());
 
         return ResponseEntity.ok(carts);
     }
 
     @PatchMapping("/{cartId}")
-    public ResponseEntity<Void> updateCart(@PathVariable Long cartId, @Valid @RequestBody CartUpdateDTO cartUpdateDTO, String userEmail) {
-        cartService.updateCart(cartId, cartUpdateDTO, userEmail);
+    public ResponseEntity<Void> updateCart(@PathVariable Long cartId, @Valid @RequestBody CartUpdateDTO cartUpdateDTO, Authentication authentication) {
+        cartService.updateCart(cartId, cartUpdateDTO, authentication.getName());
 
         return ResponseEntity.ok(null);
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> deleteCarts(@Valid @RequestBody CartDeleteListDTO cartDeleteListDTO, String
-            userEmail) {
-        cartService.deleteCarts(cartDeleteListDTO, userEmail);
+    public ResponseEntity<Void> deleteCarts(@Valid @RequestBody CartDeleteListDTO cartDeleteListDTO, Authentication authentication) {
+        cartService.deleteCarts(cartDeleteListDTO, authentication.getName());
 
         return ResponseEntity.ok(null);
     }
--- travel/src/main/java/com/travel/order/controller/OrderController.java
+++ travel/src/main/java/com/travel/order/controller/OrderController.java
@@ -8,6 +8,7 @@ import com.travel.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -22,28 +23,28 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<Void> createOrder(@Valid @RequestBody OrderCreateListDTO orderCreateListDTO, String userEmail) {
-        orderService.createOrder(orderCreateListDTO, userEmail);
+    public ResponseEntity<Void> createOrder(@Valid @RequestBody OrderCreateListDTO orderCreateListDTO, Authentication authentication) {
+        orderService.createOrder(orderCreateListDTO, authentication.getName());
 
         return ResponseEntity.ok(null);
     }
 
     @GetMapping
-    public ResponseEntity<PageResponseDTO> getOrders(@RequestParam(required = false, defaultValue = "1") int page, String userEmail) {
+    public ResponseEntity<PageResponseDTO> getOrders(@RequestParam(required = false, defaultValue = "1") int page, Authentication authentication) {
         if (page < 1) {
             throw new GlobalException(GlobalExceptionType.PAGE_INDEX_NOT_POSITIVE_NUMBER);
         }
 
         PageRequest pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
 
-        PageResponseDTO orders = orderService.getOrders(pageRequest, userEmail);
+        PageResponseDTO orders = orderService.getOrders(pageRequest, authentication.getName());
 
         return ResponseEntity.ok(orders);
     }
 
     @DeleteMapping("/{orderId}")
-    public ResponseEntity<Void> deleteOrder(@PathVariable Long orderId, String userEmail) {
-        orderService.deleteOrder(orderId, userEmail);
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long orderId, Authentication authentication) {
+        orderService.deleteOrder(orderId, authentication.getName());
 
         return ResponseEntity.ok(null);
     }
