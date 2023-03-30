f35ad5d845b8e8dfc3fc769016d133b7b482f2a7
JeYeongR <wpdud2003@gmail.com>
2023-03-31 01:14:01 +0900

    feat: #78 QnAResponseDTO 쿼리 메서드 추가

--- travel/src/main/java/com/travel/post/repository/QnARepository.java
+++ travel/src/main/java/com/travel/post/repository/QnARepository.java
@@ -1,9 +1,14 @@
 package com.travel.post.repository;
 
+import com.travel.member.entity.Member;
 import com.travel.post.entity.QnAPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QnARepository extends JpaRepository<QnAPost, Long> {
+
+    List<QnAPost> findByMember(Member member);
 }
