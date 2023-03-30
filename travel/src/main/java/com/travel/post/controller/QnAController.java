7d9b681b4d5c5257255810b07775545a2aae40da
JeYeongR <wpdud2003@gmail.com>
2023-03-30 15:58:18 +0900

    feat: #78 QnAController에 qna 작성 (createQnA) API 구현

--- travel/src/main/java/com/travel/post/controller/QnAController.java
+++ travel/src/main/java/com/travel/post/controller/QnAController.java
@@ -1,7 +1,12 @@
 package com.travel.post.controller;
 
+import com.travel.post.dto.QnACreateDTO;
 import com.travel.post.service.QnAService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class QnAController {
 
     private final QnAService qnAService;
+
+    @PostMapping
+    public ResponseEntity<Void> createQnA(@RequestBody QnACreateDTO qnACreateDTO, Authentication authentication) {
+        qnAService.createQnA(qnACreateDTO, authentication.getName());
+
+        return ResponseEntity.ok(null);
+    }
 }
