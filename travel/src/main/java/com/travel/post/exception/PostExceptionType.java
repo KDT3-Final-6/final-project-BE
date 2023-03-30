604f9da0736fe3d83542c9b2621aa6f9ba29b46d
JeYeongR <wpdud2003@gmail.com>
2023-03-30 16:36:10 +0900

    feat: #78 Post 커스텀 예외 추가

--- travel/src/main/java/com/travel/post/exception/PostExceptionType.java
+++ travel/src/main/java/com/travel/post/exception/PostExceptionType.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 public enum PostExceptionType implements CustomExceptionType {
 
     INQUIRTY_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 문의 유형입니다."),
-    PRODUCT_INQUIRY_REQUIRES_PRODUCT_NUM(HttpStatus.CONFLICT, "상품문의는 상품 번호가 필요합니다.");
+    PRODUCT_INQUIRY_REQUIRES_PRODUCT_NUM(HttpStatus.CONFLICT, "상품문의는 상품 번호가 필요합니다."),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 포스트입니다.");
 
     private final HttpStatus httpStatus;
     private final String errorMsg;
