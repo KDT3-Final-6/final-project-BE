4c28adc465df439ab7ad5f3de75492b5131a7937
lala9663 <lala96632040@gmail.com>
2023-03-30 23:27:47 +0900

    refactoring: CustomMemberDetailsService 변경

--- travel/src/main/java/com/travel/auth/service/CustomMemberDetailsService.java
+++ travel/src/main/java/com/travel/auth/service/CustomMemberDetailsService.java
@@ -1,16 +1,13 @@
 package com.travel.auth.service;
 
-import com.travel.member.repository.MemberRepository;
 import com.travel.member.entity.Member;
+import com.travel.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
 
 @Service
 @RequiredArgsConstructor
