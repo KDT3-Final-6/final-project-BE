499acefd4d102790088763ae5b95e8e67616521b
JeYeongR <wpdud2003@gmail.com>
2023-03-30 15:48:55 +0900

    feat: #78 QnAService에 qna 작성 (createQnA) 기능 구현

--- travel/src/main/java/com/travel/post/service/QnAService.java
+++ travel/src/main/java/com/travel/post/service/QnAService.java
@@ -1,4 +1,8 @@
 package com.travel.post.service;
 
+import com.travel.post.dto.QnACreateDTO;
+
 public interface QnAService {
+
+    void createQnA(QnACreateDTO qnACreateDTO, String memberEmail);
 }
--- travel/src/main/java/com/travel/post/service/impl/QnAServiceImpl.java
+++ travel/src/main/java/com/travel/post/service/impl/QnAServiceImpl.java
@@ -1,10 +1,27 @@
 package com.travel.post.service.impl;
 
+import com.travel.member.entity.Member;
+import com.travel.member.exception.MemberException;
+import com.travel.member.exception.MemberExceptionType;
+import com.travel.member.repository.MemberRepository;
+import com.travel.post.dto.QnACreateDTO;
+import com.travel.post.entity.InquiryType;
+import com.travel.post.entity.QnAPost;
+import com.travel.post.entity.QnAProductPost;
+import com.travel.post.exception.PostException;
+import com.travel.post.exception.PostExceptionType;
+import com.travel.post.repository.QnAProductRepository;
 import com.travel.post.repository.QnARepository;
 import com.travel.post.service.QnAService;
+import com.travel.product.entity.PurchasedProduct;
+import com.travel.product.exception.ProductException;
+import com.travel.product.exception.ProductExceptionType;
+import com.travel.product.repository.PurchasedProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Stream;
 
 @Service
 @Transactional
@@ -12,4 +29,43 @@ import org.springframework.transaction.annotation.Transactional;
 public class QnAServiceImpl implements QnAService {
 
     private final QnARepository qnARepository;
+    private final QnAProductRepository qnAProductRepository;
+    private final MemberRepository memberRepository;
+    private final PurchasedProductRepository purchasedProductRepository;
+
+    @Override
+    public void createQnA(QnACreateDTO qnACreateDTO, String memberEmail) {
+        Member member = memberRepository.findByMemberEmail(memberEmail)
+                .orElseThrow(() -> new MemberException(MemberExceptionType.MEMBER_NOT_FOUND));
+
+        InquiryType inquiryType = getInquiryType(qnACreateDTO.getInquiryType());
+
+        QnAPost qnAPost = new QnAPost(qnACreateDTO.getTitle(), qnACreateDTO.getContent(), member, inquiryType);
+
+        if (inquiryType.equals(InquiryType.PRODUCT)) {
+            if (qnACreateDTO.getPurchasedProductId() == null) {
+                throw new PostException(PostExceptionType.PRODUCT_INQUIRY_REQUIRES_PRODUCT_NUM);
+            }
+
+            PurchasedProduct purchasedProduct = purchasedProductRepository.findById(qnACreateDTO.getPurchasedProductId())
+                    .orElseThrow(() -> new ProductException(ProductExceptionType.PRODUCT_NOT_FOUND));
+
+            qnAProductRepository.save(new QnAProductPost(qnAPost, purchasedProduct));
+        } else {
+            qnARepository.save(qnAPost);
+        }
+    }
+
+    private InquiryType getInquiryType(String inquiry) {
+        InquiryType inquiryType = Stream.of(InquiryType.values())
+                .filter(type -> inquiry.equals(type.getKorean()))
+                .findFirst()
+                .orElse(null);
+
+        if (inquiryType == null) {
+            throw new PostException(PostExceptionType.INQUIRTY_TYPE_NOT_FOUND);
+        }
+
+        return inquiryType;
+    }
 }
