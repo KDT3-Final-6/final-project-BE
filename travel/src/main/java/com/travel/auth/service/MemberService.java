82ac7de65b6f3591d7e6575508b4721719fe5d77
lala9663 <lala96632040@gmail.com>
2023-03-31 20:36:25 +0900

    refactor: 취미 여러개로 받기 수정

--- travel/src/main/java/com/travel/auth/service/MemberService.java
+++ travel/src/main/java/com/travel/auth/service/MemberService.java
@@ -27,8 +27,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -50,6 +53,7 @@ public class MemberService {
 
         MemberImage defaultImage = MemberImage.CreateDefaultMemberImage();
 
+
         Member member = Member.builder()
                 .memberEmail(signUp.getMemberEmail())
                 .memberPassword(passwordEncoder.encode(signUp.getMemberPassword()))
@@ -57,7 +61,7 @@ public class MemberService {
                 .memberNickname(signUp.getMemberNickname())
                 .memberPhone(signUp.getMemberPhone())
                 .memberBirthDate(signUp.getMemberBirthDate())
-                .memberHobby(Hobby.valueOf(signUp.getMemberHobby().toString()))
+                .memberHobby(signUp.getMemberHobby())
                 .memberGender(signUp.getMemberGender())
                 .roles(Collections.singletonList(Authority.ROLE_USER.name()))
                 .build();
