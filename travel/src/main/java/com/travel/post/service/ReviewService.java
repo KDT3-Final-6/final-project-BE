f0d226cc5d6085cc1a32563d063dacb13ac76b9a
JeYeongR <wpdud2003@gmail.com>
2023-04-01 23:41:46 +0900

    feat: #94 ReviewService에 멤버 리뷰 목록 조회 (getReviewsByMember) 기능 구현

--- travel/src/main/java/com/travel/post/service/ReviewService.java
+++ travel/src/main/java/com/travel/post/service/ReviewService.java
@@ -1,11 +1,15 @@
 package com.travel.post.service;
 
+import com.travel.global.response.PageResponseDTO;
 import com.travel.post.dto.request.ReviewCreateRequestDTO;
 import com.travel.post.dto.request.ReviewUpdateRequestDTO;
+import org.springframework.data.domain.Pageable;
 
 public interface ReviewService {
 
     void createReview(ReviewCreateRequestDTO reviewCreateRequestDTO, String memberEmail);
+
+    PageResponseDTO getReviewsByMember(Pageable pageable, String memberEmail);
 
     void updateReview(Long postId, ReviewUpdateRequestDTO reviewUpdateRequestDTO, String memberEmail);
 
--- travel/src/main/java/com/travel/post/service/impl/ReviewServiceImpl.java
+++ travel/src/main/java/com/travel/post/service/impl/ReviewServiceImpl.java
@@ -1,11 +1,13 @@
 package com.travel.post.service.impl;
 
+import com.travel.global.response.PageResponseDTO;
 import com.travel.member.entity.Member;
 import com.travel.member.exception.MemberException;
 import com.travel.member.exception.MemberExceptionType;
 import com.travel.member.repository.MemberRepository;
 import com.travel.post.dto.request.ReviewCreateRequestDTO;
 import com.travel.post.dto.request.ReviewUpdateRequestDTO;
+import com.travel.post.dto.response.ReviewResponseDTO;
 import com.travel.post.entity.ReviewPost;
 import com.travel.post.exception.PostException;
 import com.travel.post.exception.PostExceptionType;
@@ -16,8 +18,13 @@ import com.travel.product.exception.ProductException;
 import com.travel.product.exception.ProductExceptionType;
 import com.travel.product.repository.PurchasedProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -39,6 +46,20 @@ public class ReviewServiceImpl implements ReviewService {
         ReviewPost reviewPost = new ReviewPost(purchasedProduct.getPurchasedProductName(), reviewCreateRequestDTO.getContent(), member, purchasedProduct, reviewCreateRequestDTO.getScope());
 
         reviewRepository.save(reviewPost);
+    }
+
+    @Override
+    public PageResponseDTO getReviewsByMember(Pageable pageable, String memberEmail) {
+        Member member = memberRepository.findByMemberEmail(memberEmail)
+                .orElseThrow(() -> new MemberException(MemberExceptionType.MEMBER_NOT_FOUND));
+
+        List<ReviewPost> reviewPostList = reviewRepository.findByMember(member);
+
+        List<ReviewResponseDTO> reviewResponseDTOList = reviewPostList.stream()
+                .map(ReviewPost::toReviewResponseDTO)
+                .collect(Collectors.toList());
+
+        return new PageResponseDTO(new PageImpl<>(reviewResponseDTOList, pageable, reviewResponseDTOList.size()));
     }
 
     @Override
