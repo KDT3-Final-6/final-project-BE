1c1fa136419ca8d061e6ee3bfc6a408b92da27bc
JeYeongR <wpdud2003@gmail.com>
2023-04-02 00:29:29 +0900

    feat: #94 ReviewRepository 쿼리 메서드 추가

--- travel/src/main/java/com/travel/post/repository/ReviewRepository.java
+++ travel/src/main/java/com/travel/post/repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.travel.post.repository;
 
 import com.travel.member.entity.Member;
 import com.travel.post.entity.ReviewPost;
+import com.travel.product.entity.Product;
 import com.travel.product.entity.PurchasedProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -15,4 +16,6 @@ public interface ReviewRepository extends JpaRepository<ReviewPost, Long> {
     Optional<ReviewPost> findByPostIdAndMember(Long postId, Member member);
 
     List<ReviewPost> findByMember(Member member);
+
+    List<ReviewPost> findByPurchasedProductProduct(Product product);
 }
