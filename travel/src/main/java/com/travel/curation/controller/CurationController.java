1e8a3842cb3b50dc41c68337dc0048c977ddc96f
zoomin3022 <zoomin3022@gmail.com>
2023-04-04 17:28:01 +0900

    feat: #120 설문조사 기반 2번 큐레이션 관련 Controller 구현

---
+++ travel/src/main/java/com/travel/curation/controller/CurationController.java
@@ -0,0 +1,89 @@
+package com.travel.curation.controller;
+
+import com.travel.curation.service.CurationService;
+import com.travel.global.exception.GlobalException;
+import com.travel.global.exception.GlobalExceptionType;
+import com.travel.global.response.PageResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/curation")
+public class CurationController {
+    public static final int PAGE_SIZE = 10;
+
+    private final CurationService curationService;
+
+    @GetMapping("/detail/companion")
+    public ResponseEntity<PageResponseDTO> detailCurationByCompanion(
+            @RequestParam(required = false, defaultValue = "1") int page, Authentication authentication) {
+        if (page < 1) {
+            throw new GlobalException(GlobalExceptionType.PAGE_INDEX_NOT_POSITIVE_NUMBER);
+        }
+
+        PageRequest pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
+        return ResponseEntity.ok(curationService.detailCurationByTarget(authentication.getName(), pageRequest, "companion"));
+    }
+
+    @GetMapping("/detail/gender")
+    public ResponseEntity<PageResponseDTO> detailCurationByGender(
+            @RequestParam(required = false, defaultValue = "1") int page, Authentication authentication) {
+        if (page < 1) {
+            throw new GlobalException(GlobalExceptionType.PAGE_INDEX_NOT_POSITIVE_NUMBER);
+        }
+
+        PageRequest pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
+        return ResponseEntity.ok(curationService.detailCurationByTarget(authentication.getName(), pageRequest, "gender"));
+    }
+
+    @GetMapping("/detail/age-group")
+    public ResponseEntity<PageResponseDTO> detailCurationByAgeGroup(
+            @RequestParam(required = false, defaultValue = "1") int page, Authentication authentication) {
+        if (page < 1) {
+            throw new GlobalException(GlobalExceptionType.PAGE_INDEX_NOT_POSITIVE_NUMBER);
+        }
+
+        PageRequest pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
+        return ResponseEntity.ok(curationService.detailCurationByTarget(authentication.getName(), pageRequest, "ageGroup"));
+    }
+
+    @GetMapping("/detail/hobby")
+    public ResponseEntity<PageResponseDTO> detailCurationByHobby(
+            @RequestParam(required = false, defaultValue = "1") int page, Authentication authentication) {
+        if (page < 1) {
+            throw new GlobalException(GlobalExceptionType.PAGE_INDEX_NOT_POSITIVE_NUMBER);
+        }
+
+        PageRequest pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
+        return ResponseEntity.ok(curationService.detailCurationByTarget(authentication.getName(), pageRequest, "hobby"));
+    }
+
+    @GetMapping("/detail/religion")
+    public ResponseEntity<PageResponseDTO> detailCurationByReligion(
+            @RequestParam(required = false, defaultValue = "1") int page, Authentication authentication) {
+        if (page < 1) {
+            throw new GlobalException(GlobalExceptionType.PAGE_INDEX_NOT_POSITIVE_NUMBER);
+        }
+
+        PageRequest pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
+        return ResponseEntity.ok(curationService.detailCurationByTarget(authentication.getName(), pageRequest, "religion"));
+    }
+
+    @GetMapping("/detail/season")
+    public ResponseEntity<PageResponseDTO> detailCurationBySeason(
+            @RequestParam(required = false, defaultValue = "1") int page, Authentication authentication) {
+        if (page < 1) {
+            throw new GlobalException(GlobalExceptionType.PAGE_INDEX_NOT_POSITIVE_NUMBER);
+        }
+
+        PageRequest pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
+        return ResponseEntity.ok(curationService.detailCurationByTarget(authentication.getName(), pageRequest, "season"));
+    }
+}
