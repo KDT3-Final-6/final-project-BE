eeb40a3e51df0c382c88e5b785f8c19375585cec
lala9663 <lala96632040@gmail.com>
2023-04-04 21:01:47 +0900

    feat: admin-memberList  repo
    관리자가 회원 디테일 정보볼때 필요한 게시글/qna 수

--- travel/src/main/java/com/travel/post/repository/PostRepository.java
+++ travel/src/main/java/com/travel/post/repository/PostRepository.java
@@ -2,13 +2,20 @@ package com.travel.post.repository;
 
 import com.travel.member.entity.Member;
 import com.travel.post.entity.Post;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
     Optional<Post> findByMemberAndPostId(Member member, Long postId);
+    @Query("SELECT COUNT(p) FROM Post p WHERE type(p) IN QnAPost and p.member.memberId = :memberId")
+    Long countQnaPostsByMemberId(@Param("memberId") Long memberId);
+    @Query("SELECT COUNT(r) FROM Post r WHERE type(r) IN ReviewPost and r.member.memberId = :memberId")
+    Long countReviewPostsByMemberId(@Param("memberId") Long memberId);
+
+
 }
