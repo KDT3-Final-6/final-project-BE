134d4d4dc2c4ef4cf6fa5979012ec136cc212775
JeYeongR <wpdud2003@gmail.com>
2023-04-11 19:00:22 +0900

    refactor: #206 Order 리스트DTO 수정

--- travel/src/main/java/com/travel/order/dto/response/OrderResponseDTO.java
+++ travel/src/main/java/com/travel/order/dto/response/OrderResponseDTO.java
@@ -1,14 +1,16 @@
 package com.travel.order.dto.response;
 
-import com.travel.order.entity.OrderStatus;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
-@Setter
 @Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderResponseDTO {
 
     private Long orderId;
@@ -25,30 +27,11 @@ public class OrderResponseDTO {
 
     private LocalDateTime orderDate;
 
-    private String optionName;
+    private String periodOptionName;
 
     private Integer purchasedProductQuantity;
 
     private String orderStatus;
 
     private boolean hasReview;
-
-    @Builder
-    public OrderResponseDTO(
-            Long orderId, Long productId, Long purchasedProductId, String productName,
-            String productThumbnail, Integer productPrice, LocalDateTime orderDate, String optionName,
-            Integer purchasedProductQuantity, OrderStatus orderStatus, boolean hasReview
-    ) {
-        this.orderId = orderId;
-        this.productId = productId;
-        this.purchasedProductId = purchasedProductId;
-        this.productName = productName;
-        this.productThumbnail = productThumbnail;
-        this.productPrice = productPrice;
-        this.orderDate = orderDate;
-        this.optionName = optionName;
-        this.purchasedProductQuantity = purchasedProductQuantity;
-        this.orderStatus = orderStatus.getKorean();
-        this.hasReview = hasReview;
-    }
 }
